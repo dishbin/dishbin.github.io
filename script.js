852c5a029a121f4f37ede10cd71479410627b6e1
crener <cole.rener@outlook.com>
2021-09-16 19:47:23 -0400

    added swipe animation

---
+++ script.js
@@ -0,0 +1,9 @@
+swipe = document.getElementById('swipe');
+
+window.setTimeout(() => {
+    swipe.style.display = "block";
+}, 1500)
+
+window.setTimeout(() => {
+    swipe.style.display = "none";
+}, 4200)
